57b3dc1afbe4e48b29728033b2f9675bc1a2cc62
mnsh5 <hugo.orlando2050@gmail.com>
2024-04-20 22:34:34 -0300

    add Order and Footer component

--- 04-pizza-menu/src/components/Order.tsx
+++ 04-pizza-menu/src/components/Order.tsx
@@ -1,1 +1,17 @@
-export function Order() {}
+export function Order({
+  closeHour,
+  openHour,
+}: {
+  closeHour: number;
+  openHour: number;
+}) {
+  return (
+    <div className="order">
+      <p>
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+        online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  );
+}
