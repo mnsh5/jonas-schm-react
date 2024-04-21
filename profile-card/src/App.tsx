cbb860ced3d5cdb158c5543c8716ebcdd7f3b11c
mnsh5 <hugo.orlando2050@gmail.com>
2024-04-21 16:04:48 -0300

    add new updates

--- profile-card/src/App.tsx
+++ profile-card/src/App.tsx
@@ -1,5 +1,17 @@
+import { Avatar } from "./Avatar";
+import { Intro } from "./Intro";
+import { SkillList } from "./SkillList";
+
 function App() {
-  return <div className=""></div>;
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ profile-card/src/Avatar.tsx
@@ -0,0 +1,9 @@
+export function Avatar() {
+  return (
+    <img
+      className="avatar"
+      src="https://8szvtk.csb.app/jonas.jpeg"
+      alt="Jonas Schmedtmann"
+    />
+  );
+}
