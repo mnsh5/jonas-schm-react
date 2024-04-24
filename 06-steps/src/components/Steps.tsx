ab49c14fe1131acdd00cc4cbf059600a3bef39ae
mnsh5 <hugo.orlando2050@gmail.com>
2024-04-23 21:39:52 -0300

    add new components

--- 06-steps/src/components/Steps.tsx
+++ 06-steps/src/components/Steps.tsx
@@ -5,7 +5,7 @@ import { messages } from "../messages";
 
 export function Steps() {
   const [step, setStep] = useState<number>(1);
-  // const [isOpen, setIsOpen] = useState<boolean>(true);
+  const [isOpen, setIsOpen] = useState<boolean>(true);
 
   function handlePrevious() {
     if (step > 1) {
@@ -20,35 +20,42 @@ export function Steps() {
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
+    <>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
 
-      <StepMessage step={step}>
-        {messages[step - 1]}
-        <div className="buttons">
-          <Button
-            bgColor="#e7e7e7"
-            textColor="#333"
-            onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
-          >
-            Learn how
-          </Button>
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <div className="buttons">
+              <Button
+                bgColor="#e7e7e7"
+                textColor="#333"
+                onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
+              >
+                Learn how
+              </Button>
+            </div>
+          </StepMessage>
+
+          <div className="buttons">
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
+              <span>👈</span> Previous
+            </Button>
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
+              Next <span>👉</span>
+              <span>🤓</span>
+            </Button>
+          </div>
         </div>
-      </StepMessage>
-
-      <div className="buttons">
-        <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
-          <span>👈</span> Previous
-        </Button>
-        <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
-          Next <span>👉</span>
-          <span>🤓</span>
-        </Button>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
