e41f738c5470920929b81cd7f3159399c093620e
mnsh5 <hugo.orlando2050@gmail.com>
2024-04-26 21:21:26 -0300

    add new updates

--- 07-travel-list/src/App.tsx
+++ 07-travel-list/src/App.tsx
@@ -1,9 +1,15 @@
+import { Form } from "./components/Form";
 import Logo from "./components/Logo";
+import { PackingList } from "./components/PackingList";
+import { Stats } from "./components/Stats";
 
 function App() {
   return (
     <>
       <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </>
   );
 }
